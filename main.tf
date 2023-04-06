2d3ed45c4a4968fba75e1a3b5c43566523ec2fbd
sriharitirumala <sritirumala30@gmail.com>
2023-04-06 10:20:09 +0530

    adding vpc subnets, routes, route table associations

--- main.tf
+++ main.tf
@@ -8,6 +8,7 @@ resource "aws_rds_cluster" "main" {
   backup_retention_period = var.backup_retention_period
   preferred_backup_window = var.preferred_backup_window
   db_subnet_group_name    = aws_db_subnet_group.main.name
+  skip_final_snapshot     = true
   tags = merge(
     var.tags,
     { Name = "${var.env}-rds" }
@@ -21,6 +22,7 @@ resource "aws_rds_cluster_instance" "main" {
   instance_class     = var.instance_class
   engine             = var.engine
   engine_version     = var.engine_version
+
 }
 
 
